43abd0b47aca841f6b4a61b5e107eb6252ff4207
Md Tarique Hasan <86192062+TariqueMdHasan@users.noreply.github.com>
2025-05-03 10:40:12 +0530

    Delete server/routes/tableCol.routes.js

--- server/routes/tableCol.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const { saveColData, getColData } = require('../controllers/tableCol.controller.js')
-
-router.post('/colData', saveColData)
-router.get('/colData', getColData)
-
-module.exports = router
